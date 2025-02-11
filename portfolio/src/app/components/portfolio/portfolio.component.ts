c0fc82c8714400a489e8278901e94986af8add52
Subodh Adhikari <subodhadhikari2023@outlook.com>
2025-02-11 13:11:49 +0530

    Completed the resume component --> data completion required

--- portfolio/src/app/components/portfolio/portfolio.component.ts
+++ portfolio/src/app/components/portfolio/portfolio.component.ts
@@ -11,13 +11,43 @@ import { ProjectsService } from 'src/app/services/projects.service';
 })
 export class PortfolioComponent implements OnInit {
   projects = {} as Project[];
-
+  isfiltered: boolean = false;
+  typeScript: boolean = false;
+  angular: boolean = false;
+  isCollapsed: boolean = true;
   constructor(private titleService: Title, private projectService: ProjectsService) {
     this.titleService.setTitle('Subodh Adhikari - Portfolio');
   }
   ngOnInit(): void {
     this.projects = this.projectService.getProjects();
   }
+  filter() {
+    let filterTags: Tags[] = [];
+    if (this.typeScript) {
+      filterTags.push(Tags.TYPESCRIPT);
+    }
+    if (this.angular) {
+      filterTags.push(Tags.ANGULAR);
+    }
+    
+    
+    if (this.angular==true||this.typeScript==true) {
+      this.isfiltered=true;
+    }
+    else{
+      this.isfiltered=false;
+    }
+    this.projects = this.projectService.getProjectsbyFilter(filterTags);
 
+
+
+  }
+
+  resetFilters() {
+    this.angular = false;
+    this.typeScript = false;
+    this.projects = this.projectService.getProjects();
+    this.isfiltered=false;
+  }
 
 }
